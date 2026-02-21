6eb5ac2f95d4ebb4897440f2d49518cde4d71426
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-02-21 13:42:47 +0000

    Texture rendering for east and west

--- src/main/java/dev/perxenic/formaldehyde/impl/ContinuousTextureDynamicModel.java
+++ src/main/java/dev/perxenic/formaldehyde/impl/ContinuousTextureDynamicModel.java
@@ -5,13 +5,9 @@ import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.ItemOverrides;
-import net.minecraft.client.renderer.texture.MissingTextureAtlasSprite;
-import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.resources.model.Material;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
@@ -28,9 +24,19 @@ import java.util.List;
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class ContinuousTextureDynamicModel implements IDynamicBakedModel {
-    private static final Material MATERIAL =
-            new Material(TextureAtlas.LOCATION_BLOCKS, ResourceLocation.withDefaultNamespace("block/dirt"));
     private static final ModelProperty<BlockPos> BLOCK_POS_PROPERTY = new ModelProperty<>();
+
+    private final int BLOCK_WIDTH_X = 3;
+    private final int BLOCK_WIDTH_Y = 2;
+
+    private final int TEX_BLOCK_SIZE = 4;
+
+    private final float minU;
+    private final float maxU;
+    private final float uStep;
+    private final float minV;
+    private final float maxV;
+    private final float vStep;
 
     private final boolean useAmbientOcclusion;
     private final boolean isGui3d;
@@ -50,6 +56,13 @@ public class ContinuousTextureDynamicModel implements IDynamicBakedModel {
         this.usesBlockLight = usesBlockLight;
         this.particle = particle;
         this.overrides = overrides;
+        minU = particle.getU0();
+        uStep = (particle.getU1() - minU) / TEX_BLOCK_SIZE;
+        maxU = minU + uStep * BLOCK_WIDTH_X;
+
+        minV = particle.getV0();
+        vStep = (particle.getV1() - minV) / TEX_BLOCK_SIZE;
+        maxV = minV + vStep * BLOCK_WIDTH_Y;
     }
 
     @Override
@@ -98,11 +111,66 @@ public class ContinuousTextureDynamicModel implements IDynamicBakedModel {
     ) {
         // TODO: Temporary logging message, needs to be removed at some point
         if (!modelData.has(BLOCK_POS_PROPERTY)) Formaldehyde.LOGGER.error("Block pos property not present!");
+        BlockPos pos = modelData.get(BLOCK_POS_PROPERTY);
+
         List<BakedQuad> quads = new ArrayList<>();
+
+        quads.add(eastQuad(Math.floorMod(pos.getZ(), BLOCK_WIDTH_X), Math.floorMod(pos.getY(), BLOCK_WIDTH_Y)));
+        quads.add(westQuad(Math.floorMod(pos.getZ(), BLOCK_WIDTH_X), Math.floorMod(pos.getY(), BLOCK_WIDTH_Y)));
+
+        return quads;
+    }
+
+    public BakedQuad eastQuad(int xOffset, int yOffset) {
+
+        var u0 = maxU - uStep * (BLOCK_WIDTH_X - xOffset - 1);
+        var u1 = u0 - uStep;
+
+        var v0 = minV + vStep * yOffset;
+        var v1 = v0 + vStep;
 
         var vc = new QuadBakingVertexConsumer();
 
-        vc.setSprite(MATERIAL.sprite());
+        vc.setSprite(particle);
+        vc.setShade(true);
+        vc.setDirection(Direction.EAST);
+        vc.setTintIndex(0);
+        vc.setHasAmbientOcclusion(useAmbientOcclusion());
+
+        vc.addVertex(1, 1, 1)
+                .setColor(0xFFFFFFFF)
+                .setUv(u0, v0)
+                .setNormal(1f, 0f, 0f);
+
+        vc.addVertex(1, 0, 1)
+                .setColor(0xFFFFFFFF)
+                .setUv(u0, v1)
+                .setNormal(1f, 0f, 0f);
+
+        vc.addVertex(1, 0, 0)
+                .setColor(0xFFFFFFFF)
+                .setUv(u1, v1)
+                .setNormal(1f, 0f, 0f);
+
+        vc.addVertex(1, 1, 0)
+                .setColor(0xFFFFFFFF)
+                .setUv(u1, v0)
+                .setNormal(1f, 0f, 0f);
+
+        return vc.bakeQuad();
+    }
+
+    public BakedQuad westQuad(int xOffset, int yOffset) {
+
+        var u0 = minU + uStep * xOffset;
+        var u1 = u0 + uStep;
+
+        var v0 = minV + vStep * yOffset;
+        var v1 = v0 + vStep;
+
+        var vc = new QuadBakingVertexConsumer();
+
+        vc.setSprite(particle);
         vc.setShade(true);
         vc.setDirection(Direction.WEST);
         vc.setTintIndex(0);
@@ -110,26 +178,24 @@ public class ContinuousTextureDynamicModel implements IDynamicBakedModel {
 
         vc.addVertex(0, 1, 0)
                 .setColor(0xFFFFFFFF)
-                .setUv(MATERIAL.sprite().getU0(), MATERIAL.sprite().getV0())
+                .setUv(u0, v0)
                 .setNormal(-1f, 0f, 0f);
 
         vc.addVertex(0, 0, 0)
                 .setColor(0xFFFFFFFF)
-                .setUv(MATERIAL.sprite().getU0(), MATERIAL.sprite().getV1())
+                .setUv(u0, v1)
                 .setNormal(-1f, 0f, 0f);
 
         vc.addVertex(0, 0, 1)
                 .setColor(0xFFFFFFFF)
-                .setUv(MATERIAL.sprite().getU1(), MATERIAL.sprite().getV1())
+                .setUv(u1, v1)
                 .setNormal(-1f, 0f, 0f);
 
         vc.addVertex(0, 1, 1)
                 .setColor(0xFFFFFFFF)
-                .setUv(MATERIAL.sprite().getU1(), MATERIAL.sprite().getV0())
+                .setUv(u1, v0)
                 .setNormal(-1f, 0f, 0f);
 
-        quads.add(vc.bakeQuad());
-
-        return quads;
+        return vc.bakeQuad();
     }
 }
