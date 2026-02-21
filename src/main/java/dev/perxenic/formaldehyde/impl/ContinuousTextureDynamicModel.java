4d2271f75f454e8a5f9bd7d72032e34211a79878
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-02-21 12:32:51 +0000

    Dirt texture rendering

--- src/main/java/dev/perxenic/formaldehyde/impl/ContinuousTextureDynamicModel.java
+++ src/main/java/dev/perxenic/formaldehyde/impl/ContinuousTextureDynamicModel.java
@@ -1,43 +1,35 @@
 package dev.perxenic.formaldehyde.impl;
 
-import com.mojang.blaze3d.vertex.VertexFormatElement;
 import dev.perxenic.formaldehyde.Formaldehyde;
 import net.minecraft.MethodsReturnNonnullByDefault;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.BakedQuad;
-import net.minecraft.client.renderer.block.model.FaceBakery;
 import net.minecraft.client.renderer.block.model.ItemOverrides;
 import net.minecraft.client.renderer.texture.MissingTextureAtlasSprite;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockAndTintGetter;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.client.model.IDynamicBakedModel;
 import net.neoforged.neoforge.client.model.data.ModelData;
 import net.neoforged.neoforge.client.model.data.ModelProperty;
 import net.neoforged.neoforge.client.model.pipeline.QuadBakingVertexConsumer;
 import org.jetbrains.annotations.Nullable;
-import org.joml.Vector3f;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class ContinuousTextureDynamicModel implements IDynamicBakedModel {
-    private static final Material MISSING_TEXTURE =
-            new Material(TextureAtlas.LOCATION_BLOCKS, MissingTextureAtlasSprite.getLocation());
+    private static final Material MATERIAL =
+            new Material(TextureAtlas.LOCATION_BLOCKS, ResourceLocation.withDefaultNamespace("block/dirt"));
     private static final ModelProperty<BlockPos> BLOCK_POS_PROPERTY = new ModelProperty<>();
 
     private final boolean useAmbientOcclusion;
@@ -110,32 +102,31 @@ public class ContinuousTextureDynamicModel implements IDynamicBakedModel {
 
         var vc = new QuadBakingVertexConsumer();
 
-        vc.setSprite(MISSING_TEXTURE.sprite());
-        vc.setShade(false);
-        if (side == null) side = Direction.DOWN;
-        vc.setDirection(side);
+        vc.setSprite(MATERIAL.sprite());
+        vc.setShade(true);
+        vc.setDirection(Direction.WEST);
         vc.setTintIndex(0);
         vc.setHasAmbientOcclusion(useAmbientOcclusion());
 
-        vc.addVertex(0, 0, 0)
-                .setColor(0xFFFFFFFF)
-                .setUv(0, 0)
-                .setNormal(1f, 0f, 0f);
-
         vc.addVertex(0, 1, 0)
                 .setColor(0xFFFFFFFF)
-                .setUv(1, 0)
-                .setNormal(1f, 0f, 0f);
+                .setUv(MATERIAL.sprite().getU0(), MATERIAL.sprite().getV0())
+                .setNormal(-1f, 0f, 0f);
 
-        vc.addVertex(0, 1, 1)
+        vc.addVertex(0, 0, 0)
                 .setColor(0xFFFFFFFF)
-                .setUv(1, 1)
-                .setNormal(1f, 0f, 0f);
+                .setUv(MATERIAL.sprite().getU0(), MATERIAL.sprite().getV1())
+                .setNormal(-1f, 0f, 0f);
 
         vc.addVertex(0, 0, 1)
                 .setColor(0xFFFFFFFF)
-                .setUv(0, 1)
-                .setNormal(1f, 0f, 0f);
+                .setUv(MATERIAL.sprite().getU1(), MATERIAL.sprite().getV1())
+                .setNormal(-1f, 0f, 0f);
+
+        vc.addVertex(0, 1, 1)
+                .setColor(0xFFFFFFFF)
+                .setUv(MATERIAL.sprite().getU1(), MATERIAL.sprite().getV0())
+                .setNormal(-1f, 0f, 0f);
 
         quads.add(vc.bakeQuad());
 
