8ea471958c57a4ef2dfa7d0f3b1ba86d0958aba9
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-02-21 17:53:48 +0000

    Moved quad baking to a helper class

---
+++ src/main/java/dev/perxenic/formaldehyde/helper/QuadBaker.java
@@ -0,0 +1,213 @@
+package dev.perxenic.formaldehyde.helper;
+
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.core.Direction;
+import net.neoforged.neoforge.client.model.pipeline.QuadBakingVertexConsumer;
+
+public class QuadBaker {
+    public static BakedQuad quad(
+            TextureAtlasSprite sprite,
+            boolean shade,
+            int tintIndex,
+            boolean ambientOcclusion,
+            int color,
+            float u0, float u1,
+            float v0, float v1,
+            Direction direction,
+            float[] vertices,
+            float[] normal
+    ) {
+        var vc = new QuadBakingVertexConsumer();
+
+        vc.setSprite(sprite);
+        vc.setShade(shade);
+        vc.setDirection(direction);
+        vc.setTintIndex(tintIndex);
+        vc.setHasAmbientOcclusion(ambientOcclusion);
+
+        vc.addVertex(vertices[0], vertices[1], vertices[2])
+                .setColor(color)
+                .setUv(u0, v0)
+                .setNormal(normal[0], normal[1], normal[2]);
+
+        vc.addVertex(vertices[3], vertices[4], vertices[5])
+                .setColor(color)
+                .setUv(u0, v1)
+                .setNormal(normal[0], normal[1], normal[2]);
+
+        vc.addVertex(vertices[6], vertices[7], vertices[8])
+                .setColor(color)
+                .setUv(u1, v1)
+                .setNormal(normal[0], normal[1], normal[2]);
+
+        vc.addVertex(vertices[9], vertices[10], vertices[11])
+                .setColor(color)
+                .setUv(u1, v0)
+                .setNormal(normal[0], normal[1], normal[2]);
+
+        return vc.bakeQuad();
+    }
+
+    public final static float[] NORTH_VERTICES = {
+            1, 1, 0,
+            1, 0, 0,
+            0, 0, 0,
+            0, 1, 0
+    };
+
+    public final static float[] NORTH_NORMAL = {0, 0, -1};
+
+    public static BakedQuad northQuad(
+            TextureAtlasSprite sprite,
+            boolean shade,
+            int tintIndex,
+            boolean ambientOcclusion,
+            int color,
+            float u0, float u1,
+            float v0, float v1
+    ) {
+        return quad(
+                sprite, shade, tintIndex, ambientOcclusion, color,
+                u0, u1, v0, v1,
+                Direction.NORTH,
+                NORTH_VERTICES,
+                NORTH_NORMAL
+        );
+    }
+
+    public final static float[] SOUTH_VERTICES = {
+            0, 1, 1,
+            0, 0, 1,
+            1, 0, 1,
+            1, 1, 1
+    };
+
+    public final static float[] SOUTH_NORMAL = {0, 0, 1};
+
+    public static BakedQuad southQuad(
+            TextureAtlasSprite sprite,
+            boolean shade,
+            int tintIndex,
+            boolean ambientOcclusion,
+            int color,
+            float u0, float u1,
+            float v0, float v1
+    ) {
+        return quad(
+                sprite, shade, tintIndex, ambientOcclusion, color,
+                u0, u1, v0, v1,
+                Direction.SOUTH,
+                SOUTH_VERTICES,
+                SOUTH_NORMAL
+        );
+    }
+
+    public final static float[] WEST_VERTICES = {
+            0, 1, 0,
+            0, 0, 0,
+            0, 0, 1,
+            0, 1, 1
+    };
+
+    public final static float[] WEST_NORMAL = {-1, 0, 0};
+
+    public static BakedQuad westQuad(
+            TextureAtlasSprite sprite,
+            boolean shade,
+            int tintIndex,
+            boolean ambientOcclusion,
+            int color,
+            float u0, float u1,
+            float v0, float v1
+    ) {
+        return quad(
+                sprite, shade, tintIndex, ambientOcclusion, color,
+                u0, u1, v0, v1,
+                Direction.WEST,
+                WEST_VERTICES,
+                WEST_NORMAL
+        );
+    }
+
+    public final static float[] EAST_VERTICES = {
+            1, 1, 1,
+            1, 0, 1,
+            1, 0, 0,
+            1, 1, 0,
+    };
+
+    public final static float[] EAST_NORMAL = {1, 0, 0};
+
+    public static BakedQuad eastQuad(
+            TextureAtlasSprite sprite,
+            boolean shade,
+            int tintIndex,
+            boolean ambientOcclusion,
+            int color,
+            float u0, float u1,
+            float v0, float v1
+    ) {
+        return quad(
+                sprite, shade, tintIndex, ambientOcclusion, color,
+                u0, u1, v0, v1,
+                Direction.EAST,
+                EAST_VERTICES,
+                EAST_NORMAL
+        );
+    }
+
+    public final static float[] DOWN_VERTICES = {
+            1, 0, 0,
+            1, 0, 1,
+            0, 0, 1,
+            0, 0, 0,
+    };
+
+    public final static float[] DOWN_NORMAL = {0, -1, 0};
+
+    public static BakedQuad downQuad(
+            TextureAtlasSprite sprite,
+            boolean shade,
+            int tintIndex,
+            boolean ambientOcclusion,
+            int color,
+            float u0, float u1,
+            float v0, float v1
+    ) {
+        return quad(
+                sprite, shade, tintIndex, ambientOcclusion, color,
+                u0, u1, v0, v1,
+                Direction.DOWN,
+                DOWN_VERTICES,
+                DOWN_NORMAL
+        );
+    }
+
+    public final static float[] UP_VERTICES = {
+            0, 1, 0,
+            0, 1, 1,
+            1, 1, 1,
+            1, 1, 0,
+    };
+
+    public final static float[] UP_NORMAL = {0, 1, 0};
+
+    public static BakedQuad upQuad(
+            TextureAtlasSprite sprite,
+            boolean shade,
+            int tintIndex,
+            boolean ambientOcclusion,
+            int color,
+            float u0, float u1,
+            float v0, float v1
+    ) {
+        return quad(
+                sprite, shade, tintIndex, ambientOcclusion, color,
+                u0, u1, v0, v1,
+                Direction.UP,
+                UP_VERTICES,
+                UP_NORMAL
+        );
+    }
+}
--- src/main/java/dev/perxenic/formaldehyde/impl/ContinuousTextureDynamicModel.java
+++ src/main/java/dev/perxenic/formaldehyde/impl/ContinuousTextureDynamicModel.java
@@ -1,6 +1,6 @@
 package dev.perxenic.formaldehyde.impl;
 
-import dev.perxenic.formaldehyde.Formaldehyde;
+import dev.perxenic.formaldehyde.helper.QuadBaker;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.BakedQuad;
@@ -14,7 +14,6 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.client.model.IDynamicBakedModel;
 import net.neoforged.neoforge.client.model.data.ModelData;
 import net.neoforged.neoforge.client.model.data.ModelProperty;
-import net.neoforged.neoforge.client.model.pipeline.QuadBakingVertexConsumer;
 import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -159,271 +158,93 @@ public class ContinuousTextureDynamicModel implements IDynamicBakedModel {
 
         if (side == null) return quads;
 
-        if (side == Direction.NORTH)
-            quads.add(northQuad(
-                    Math.floorMod(-pos.getX(), directionTileSize.northU),
-                    Math.floorMod(pos.getY(), directionTileSize.northV)
-            ));
-        if (side == Direction.SOUTH)
-            quads.add(southQuad(
-                    Math.floorMod(-pos.getX(), directionTileSize.southU),
-                    Math.floorMod(pos.getY(), directionTileSize.southV)
-            ));
+        if (side == Direction.NORTH) {
+            float u0 = north.getU0() + northUStep * Math.floorMod(-pos.getX() - 1, directionTileSize.northU);
+            float v0 = north.getV0() + northVStep * Math.floorMod(-pos.getY(), directionTileSize.northV);
 
-        if (side == Direction.WEST)
-            quads.add(westQuad(
-                    Math.floorMod(pos.getZ(), directionTileSize.westU),
-                    Math.floorMod(pos.getY(), directionTileSize.westV)
+            quads.add(QuadBaker.northQuad(
+                    north,
+                    true,
+                    0,
+                    useAmbientOcclusion(),
+                    0xFFFFFFFF,
+                    u0, u0 + northUStep,
+                    v0, v0 + northVStep
             ));
-        if (side == Direction.EAST)
-            quads.add(eastQuad(
-                    Math.floorMod(pos.getZ(), directionTileSize.eastU),
-                    Math.floorMod(pos.getY(), directionTileSize.eastV)
-            ));
+        }
+        if (side == Direction.SOUTH) {
+            float u0 = south.getU0() + southUStep * Math.floorMod(pos.getX(), directionTileSize.southU);
+            float v0 = south.getV0() + southVStep * Math.floorMod(-pos.getY(), directionTileSize.southV);
 
-        if (side == Direction.DOWN)
-            quads.add(downQuad(
-                    Math.floorMod(pos.getZ(), directionTileSize.downU),
-                    Math.floorMod(pos.getX(), directionTileSize.downV)
+            quads.add(QuadBaker.southQuad(
+                    south,
+                    true,
+                    0,
+                    useAmbientOcclusion(),
+                    0xFFFFFFFF,
+                    u0, u0 + southUStep,
+                    v0, v0 + southVStep
             ));
-        if (side == Direction.UP)
-            quads.add(upQuad(
-                    Math.floorMod(pos.getZ(), directionTileSize.upU),
-                    Math.floorMod(pos.getX(), directionTileSize.upV)
+        }
+
+        if (side == Direction.WEST) {
+            float u0 = west.getU0() + westUStep * Math.floorMod(pos.getZ() - 1, directionTileSize.westU);
+            float v0 = west.getV0() + westVStep * Math.floorMod(-pos.getY(), directionTileSize.westV);
+
+            quads.add(QuadBaker.westQuad(
+                    west,
+                    true,
+                    0,
+                    useAmbientOcclusion(),
+                    0xFFFFFFFF,
+                    u0, u0 + westUStep,
+                    v0, v0 + westVStep
             ));
+        }
+        if (side == Direction.EAST) {
+            float u0 = east.getU0() + eastUStep * Math.floorMod(-pos.getZ(), directionTileSize.eastU);
+            float v0 = east.getV0() + eastVStep * Math.floorMod(-pos.getY(), directionTileSize.eastV);
+
+            quads.add(QuadBaker.eastQuad(
+                    east,
+                    true,
+                    0,
+                    useAmbientOcclusion(),
+                    0xFFFFFFFF,
+                    u0, u0 + eastUStep,
+                    v0, v0 + eastVStep
+            ));
+        }
+
+        if (side == Direction.DOWN) {
+            float u0 = down.getU0() + downUStep * Math.floorMod(-pos.getX(), directionTileSize.downU);
+            float v0 = down.getV0() + downVStep * Math.floorMod(pos.getZ(), directionTileSize.downV);
+
+            quads.add(QuadBaker.downQuad(
+                    down,
+                    true,
+                    0,
+                    useAmbientOcclusion(),
+                    0xFFFFFFFF,
+                    u0, u0 + downUStep,
+                    v0, v0 + downVStep
+            ));
+        }
+        if (side == Direction.UP) {
+            float u0 = up.getU0() + upUStep * Math.floorMod(pos.getX(), directionTileSize.upU);
+            float v0 = up.getV0() + upVStep * Math.floorMod(pos.getZ() - 1, directionTileSize.upV);
+
+            quads.add(QuadBaker.upQuad(
+                    up,
+                    true,
+                    0,
+                    useAmbientOcclusion(),
+                    0xFFFFFFFF,
+                    u0, u0 + upUStep,
+                    v0, v0 + upVStep
+            ));
+        }
 
         return quads;
-    }
-
-    public BakedQuad northQuad(int uOffset, int vOffset) {
-        var u0 = north.getU0() + northUStep * uOffset;
-        var u1 = u0 + northUStep;
-
-        var v0 = north.getV0() + northVStep * vOffset;
-        var v1 = v0 + northVStep;
-
-        var vc = new QuadBakingVertexConsumer();
-
-        vc.setSprite(north);
-        vc.setShade(true);
-        vc.setDirection(Direction.NORTH);
-        vc.setTintIndex(0);
-        vc.setHasAmbientOcclusion(useAmbientOcclusion());
-
-        vc.addVertex(1, 1, 0)
-                .setColor(0xFFFFFFFF)
-                .setUv(u0, v0)
-                .setNormal(0f, 0f, -1f);
-
-        vc.addVertex(1, 0, 0)
-                .setColor(0xFFFFFFFF)
-                .setUv(u0, v1)
-                .setNormal(0f, 0f, -1f);
-
-        vc.addVertex(0, 0, 0)
-                .setColor(0xFFFFFFFF)
-                .setUv(u1, v1)
-                .setNormal(0f, 0f, -1f);
-
-        vc.addVertex(0, 1, 0)
-                .setColor(0xFFFFFFFF)
-                .setUv(u1, v0)
-                .setNormal(0f, 0f, -1f);
-
-        return vc.bakeQuad();
-    }
-
-    public BakedQuad southQuad(int uOffset, int vOffset) {
-        // Flip texture horizontally for south quad
-        var u0 = south.getU0() + southUStep * (directionTileSize.southU - uOffset - 1);
-        var u1 = u0 + southUStep;
-
-        var v0 = south.getV0() + southVStep * vOffset;
-        var v1 = v0 + southVStep;
-
-        var vc = new QuadBakingVertexConsumer();
-
-        vc.setSprite(south);
-        vc.setShade(true);
-        vc.setDirection(Direction.SOUTH);
-        vc.setTintIndex(0);
-        vc.setHasAmbientOcclusion(useAmbientOcclusion());
-
-        vc.addVertex(0, 1, 1)
-                .setColor(0xFFFFFFFF)
-                .setUv(u0, v0)
-                .setNormal(0f, 0f, 1f);
-
-        vc.addVertex(0, 0, 1)
-                .setColor(0xFFFFFFFF)
-                .setUv(u0, v1)
-                .setNormal(0f, 0f, 1f);
-
-        vc.addVertex(1, 0, 1)
-                .setColor(0xFFFFFFFF)
-                .setUv(u1, v1)
-                .setNormal(0f, 0f, 1f);
-
-        vc.addVertex(1, 1, 1)
-                .setColor(0xFFFFFFFF)
-                .setUv(u1, v0)
-                .setNormal(0f, 0f, 1f);
-
-        return vc.bakeQuad();
-    }
-
-    public BakedQuad westQuad(int uOffset, int vOffset) {
-        var u0 = west.getU0() + westUStep * uOffset;
-        var u1 = u0 + westUStep;
-
-        var v0 = west.getV0() + westVStep * vOffset;
-        var v1 = v0 + westVStep;
-
-        var vc = new QuadBakingVertexConsumer();
-
-        vc.setSprite(west);
-        vc.setShade(true);
-        vc.setDirection(Direction.WEST);
-        vc.setTintIndex(0);
-        vc.setHasAmbientOcclusion(useAmbientOcclusion());
-
-        vc.addVertex(0, 1, 0)
-                .setColor(0xFFFFFFFF)
-                .setUv(u0, v0)
-                .setNormal(-1f, 0f, 0f);
-
-        vc.addVertex(0, 0, 0)
-                .setColor(0xFFFFFFFF)
-                .setUv(u0, v1)
-                .setNormal(-1f, 0f, 0f);
-
-        vc.addVertex(0, 0, 1)
-                .setColor(0xFFFFFFFF)
-                .setUv(u1, v1)
-                .setNormal(-1f, 0f, 0f);
-
-        vc.addVertex(0, 1, 1)
-                .setColor(0xFFFFFFFF)
-                .setUv(u1, v0)
-                .setNormal(-1f, 0f, 0f);
-
-        return vc.bakeQuad();
-    }
-
-    public BakedQuad eastQuad(int uOffset, int vOffset) {
-        // Flip texture horizontally for east quad
-        var u0 = east.getU0() + eastUStep * (directionTileSize.eastU - uOffset - 1);
-        var u1 = u0 + eastUStep;
-
-
-        var v0 = east.getV0() + eastVStep * vOffset;
-        var v1 = v0 + eastVStep;
-
-        var vc = new QuadBakingVertexConsumer();
-
-        vc.setSprite(east);
-        vc.setShade(true);
-        vc.setDirection(Direction.EAST);
-        vc.setTintIndex(0);
-        vc.setHasAmbientOcclusion(useAmbientOcclusion());
-
-        vc.addVertex(1, 1, 1)
-                .setColor(0xFFFFFFFF)
-                .setUv(u0, v0)
-                .setNormal(1f, 0f, 0f);
-
-        vc.addVertex(1, 0, 1)
-                .setColor(0xFFFFFFFF)
-                .setUv(u0, v1)
-                .setNormal(1f, 0f, 0f);
-
-        vc.addVertex(1, 0, 0)
-                .setColor(0xFFFFFFFF)
-                .setUv(u1, v1)
-                .setNormal(1f, 0f, 0f);
-
-        vc.addVertex(1, 1, 0)
-                .setColor(0xFFFFFFFF)
-                .setUv(u1, v0)
-                .setNormal(1f, 0f, 0f);
-
-        return vc.bakeQuad();
-    }
-
-    public BakedQuad downQuad(int uOffset, int vOffset) {
-        // Flip texture horizontally for down quad
-        var u0 = down.getU0() + downUStep * (directionTileSize.downU - uOffset - 1);
-        var u1 = u0 + downUStep;
-
-        var v0 = down.getV0() + downVStep * vOffset;
-        var v1 = v0 + downVStep;
-
-        var vc = new QuadBakingVertexConsumer();
-
-        vc.setSprite(down);
-        vc.setShade(true);
-        vc.setDirection(Direction.DOWN);
-        vc.setTintIndex(0);
-        vc.setHasAmbientOcclusion(useAmbientOcclusion());
-
-        vc.addVertex(0, 0, 1)
-                .setColor(0xFFFFFFFF)
-                .setUv(u0, v1)
-                .setNormal(0f, -1f, 0f);
-
-        vc.addVertex(0, 0, 0)
-                .setColor(0xFFFFFFFF)
-                .setUv(u1, v1)
-                .setNormal(0f, -1f, 0f);
-
-        vc.addVertex(1, 0, 0)
-                .setColor(0xFFFFFFFF)
-                .setUv(u1, v0)
-                .setNormal(0f, -1f, 0f);
-
-        vc.addVertex(1, 0, 1)
-                .setColor(0xFFFFFFFF)
-                .setUv(u0, v0)
-                .setNormal(0f, -1f, 0f);
-
-        return vc.bakeQuad();
-    }
-
-    public BakedQuad upQuad(int uOffset, int vOffset) {
-        var u0 = up.getU0() + upUStep * uOffset;
-        var u1 = u0 + upUStep;
-
-        var v0 = up.getV0() + upVStep * vOffset;
-        var v1 = v0 + upVStep;
-
-        var vc = new QuadBakingVertexConsumer();
-
-        vc.setSprite(up);
-        vc.setShade(true);
-        vc.setDirection(Direction.UP);
-        vc.setTintIndex(0);
-        vc.setHasAmbientOcclusion(useAmbientOcclusion());
-
-        vc.addVertex(0, 1, 0)
-                .setColor(0xFFFFFFFF)
-                .setUv(u0, v1)
-                .setNormal(0f, 0f, 1f);
-
-        vc.addVertex(0, 1, 1)
-                .setColor(0xFFFFFFFF)
-                .setUv(u1, v1)
-                .setNormal(0f, 0f, 1f);
-
-        vc.addVertex(1, 1, 1)
-                .setColor(0xFFFFFFFF)
-                .setUv(u1, v0)
-                .setNormal(0f, 0f, 1f);
-
-        vc.addVertex(1, 1, 0)
-                .setColor(0xFFFFFFFF)
-                .setUv(u0, v0)
-                .setNormal(0f, 0f, 1f);
-
-        return vc.bakeQuad();
     }
 }
