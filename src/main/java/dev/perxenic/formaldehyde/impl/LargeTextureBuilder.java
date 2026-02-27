1ecd7299bfbc56a2cb816149ef97ab710e2029f6
Auseawesome <ajthomasbtb@outlook.com>
2026-02-27 18:18:44 +0000

    Completed large texture builder

--- src/main/java/dev/perxenic/formaldehyde/impl/LargeTextureBuilder.java
+++ src/main/java/dev/perxenic/formaldehyde/impl/LargeTextureBuilder.java
@@ -11,6 +11,9 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class LargeTextureBuilder extends CustomLoaderBuilder<BlockModelBuilder> {
+    private FacewiseTextureSize _texSizes;
+    private FacewiseTileDimensions _tileDims;
+
     public LargeTextureBuilder(BlockModelBuilder parent, ExistingFileHelper existingFileHelper) {
         super(
                 LargeTextureGeometryLoader.ID,
@@ -20,8 +23,55 @@ public class LargeTextureBuilder extends CustomLoaderBuilder<BlockModelBuilder> 
         );
     }
 
+    public LargeTextureBuilder setTextureSizes(FacewiseTextureSize texSizes) {
+        _texSizes = texSizes;
+        return this;
+    }
+
+    public LargeTextureBuilder setTextureSizes(int down, int up, int north, int east, int south, int west) {
+        _texSizes = new FacewiseTextureSize(down, up, north, east, south, west);
+        return this;
+    }
+
+    public LargeTextureBuilder setTileDimensions(FacewiseTileDimensions tileDims) {
+        _tileDims = tileDims;
+        return this;
+    }
+
+    public LargeTextureBuilder setTileDimensions(
+            int downU, int downV,
+            int upU, int upV,
+            int northU, int northV,
+            int eastU, int eastV,
+            int southU, int southV,
+            int westU, int westV
+    ) {
+        _tileDims = new FacewiseTileDimensions(
+                downU, downV,
+                upU, upV,
+                northU, northV,
+                eastU, eastV,
+                southU, southV,
+                westU, westV
+        );
+        return this;
+    }
+
     @Override
     public JsonObject toJson(JsonObject json) {
-        return super.toJson(json);
+        json = super.toJson(json);
+
+        if (json.has("elements"))
+            throw new IllegalStateException("Large texture model currently does not support custom model elements!");
+
+        if (_texSizes == null)
+            throw new IllegalStateException("Texture sizes were never assigned!");
+        if (_tileDims == null)
+            throw new IllegalStateException("Tile dimensions were never assigned!");
+
+        json.add("texture_size", _texSizes.toJson());
+        json.add("tile_size", _tileDims.toJson());
+
+        return json;
     }
 }
